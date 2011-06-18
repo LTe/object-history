5a311ef9e7add61233ef63dc185601982a68c63e
Piotr Niełacny <piotr.nielacny@gmail.com>
2011-06-18 20:23:26 +0200

    fix matcher for nil class

--- lib/object-history/rspec-matchers.rb
+++ lib/object-history/rspec-matchers.rb
@@ -1,13 +1,25 @@
 RSpec::Matchers.define :have_track do |accessor, expected|
   match do |actual|
-    actual.__history.map{|x|x.send(accessor)} == expected
+    if actual.__history
+      actual.__history.map{|x|x.send(accessor)} == expected
+    else
+      [] == expected
+    end
   end
 
   failure_message_for_should do |actual|
-    "expected that #{actual.__history.map{|x|x.send(accessor)}} would be a precise multiple of #{expected}"
+    if actual.__history
+      "expected that #{actual.__history.map{|x|x.send(accessor)}} would be a precise multiple of #{expected}"
+    else
+      "expected that #{[]} would be a precise multiple of #{expected}"
+    end
   end
 
   failure_message_for_should_not do |actual|
-    "expected that #{actual.__history.map{|x|x.send(accessor)}} would not be a precise multiple of #{expected}"
+    if actual.__history
+      "expected that #{actual.__history.map{|x|x.send(accessor)}} would not be a precise multiple of #{expected}"
+    else
+      "expected that #{[]} would not be a precise multiple of #{expected}"
+    end
   end
 end
